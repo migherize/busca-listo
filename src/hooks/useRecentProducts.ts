8af0bac697b7438dc487dcedc08e7eeed9bf5536
Miguel Andres Herize Godoy <mherize@ia.com.mx>
2025-08-21 00:50:19 -0400

    add hardcode render

--- src/hooks/useRecentProducts.ts
+++ src/hooks/useRecentProducts.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import type { Product } from "@shared/schema";
 
-const API_HOST = import.meta.env.VITE_API_HOST;
+// const API_HOST = import.meta.env.VITE_API_HOST;
+const API_HOST = "https://buscalistobackend.onrender.com";
 
 export function useRecentProducts() {
   return useQuery<Product[]>({
